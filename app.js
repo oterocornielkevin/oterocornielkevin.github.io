9c320d8e0e47e2150ac966694f966ad36c099cef
Kevin Otero <oterocornielkevin@gmail.com>
2020-03-31 20:31:02 -0400

    Removed animations from mobile

--- app.js
+++ app.js
@@ -2,80 +2,78 @@ $(document).ready(function(){
 
     //Event handler for setting the active nav-item in the navigation bar.
     $(window).scroll(function() {
-        var aboutmeTop = $('#aboutme-section').offset().top,
-            aboutmeHeight = $('#aboutme-section').outerHeight(),
-            skillsTop = $('#aboutme-skills').offset().top - 100,
-            skillsHeight = $('#aboutme-skills').outerHeight(),
-            portfolioTop = $('#portfolio-section').offset().top - 71,
-            portfolioHeight = $('#portfolio-section').outerHeight(),
-            contactmeTop = $('#contactme-section').offset().top - 40,
-            contactmeHeight = $('#contactme-section').outerHeight(),
-            windowHeight = $(window).height(),
-            windowScroll = $(this).scrollTop();
 
-        //About Me section
-        if(windowScroll > (aboutmeTop + aboutmeHeight - windowHeight)){
-            if(!$('#aboutme-nav').hasClass('active')){
-                $('#aboutme-nav').addClass('active');
-            } 
+        let media = window.matchMedia('(min-width: 992px)');
 
-            if($('#portfolio-nav').hasClass('active')){
-                $('#portfolio-nav').removeClass('active');
+        if(media.matches){
+                let aboutmeTop = $('#aboutme-section').offset().top,
+                aboutmeHeight = $('#aboutme-section').outerHeight(),
+                portfolioTop = $('#portfolio-section').offset().top - 71,
+                portfolioHeight = $('#portfolio-section').outerHeight(),
+                contactmeTop = $('#contactme-section').offset().top - 40,
+                contactmeHeight = $('#contactme-section').outerHeight(),
+                windowHeight = $(window).height(),
+                windowScroll = $(this).scrollTop();
+
+            //About Me section
+            if(windowScroll > (aboutmeTop + aboutmeHeight - windowHeight)){
+                if(!$('#aboutme-nav').hasClass('active')){
+                    $('#aboutme-nav').addClass('active');
+                } 
+
+                if($('#portfolio-nav').hasClass('active')){
+                    $('#portfolio-nav').removeClass('active');
+                }
+
+                if($('#contactme-nav').hasClass('active')){
+                    $('#contactme-nav').removeClass('active');
+                }
+            }
+            
+            //Portfolio section
+            if(windowScroll > (portfolioTop + portfolioHeight - windowHeight)){
+                if(!$('#portfolio-nav').hasClass('active')){
+                    $('#portfolio-nav').addClass('active');
+                }
+
+                if($('#aboutme-nav').hasClass('active')){
+                    $('#aboutme-nav').removeClass('active');
+                }
+
+                if($('#contactme-nav').hasClass('active')){
+                    $('#contactme-nav').removeClass('active');
+                }
+
+                $('#portfolio-header').addClass('animate-header');
+                $('.card').addClass('animate-card');
             }
 
-            if($('#contactme-nav').hasClass('active')){
-                $('#contactme-nav').removeClass('active');
+            //Contact Me section
+            if(windowScroll > (contactmeTop + contactmeHeight - windowHeight)){
+                if(!$('#contactme-nav').hasClass('active')){
+                    $('#contactme-nav').addClass('active');
+                }
+
+                if($('#portfolio-nav').hasClass('active')){
+                    $('#portfolio-nav').removeClass('active');
+                }
+
+                if($('#aboutme-nav').hasClass('active')){
+                    $('#aboutme-nav').removeClass('active');
+                }
+
+                $('#contactme-header').addClass('animate-header');
+                $('.contactme-input').addClass('animate-form');
+                $('.sendmessage').addClass('animate-button');
             }
         }
 
-        //Skills column is visible
-        if(windowScroll > (skillsTop + skillsHeight - windowHeight)) {
-            $('#skills-header').addClass('skills-header-animation');
-            $('#skills-header-underline').addClass('skills-header-animation');
-            $('.skill-item').addClass('skill-animation');
-        }
         
-        //Portfolio section
-        if(windowScroll > (portfolioTop + portfolioHeight - windowHeight)){
-            if(!$('#portfolio-nav').hasClass('active')){
-                $('#portfolio-nav').addClass('active');
-            }
-
-            if($('#aboutme-nav').hasClass('active')){
-                $('#aboutme-nav').removeClass('active');
-            }
-
-            if($('#contactme-nav').hasClass('active')){
-                $('#contactme-nav').removeClass('active');
-            }
-
-            $('#portfolio-header').addClass('animate-header');
-            $('.card').addClass('animate-card');
-        }
-
-        //Contact Me section
-        if(windowScroll > (contactmeTop + contactmeHeight - windowHeight)){
-            if(!$('#contactme-nav').hasClass('active')){
-                $('#contactme-nav').addClass('active');
-            }
-
-            if($('#portfolio-nav').hasClass('active')){
-                $('#portfolio-nav').removeClass('active');
-            }
-
-            if($('#aboutme-nav').hasClass('active')){
-                $('#aboutme-nav').removeClass('active');
-            }
-
-            $('#contactme-header').addClass('animate-header');
-            $('.contactme-input').addClass('animate-form');
-            $('.sendmessage').addClass('animate-button');
-        }
     });
 
     //Event handler for clicking the navigation items.
     $('#navbarSupportedContent li a').click(function(e){
-        var targetHref = $(this).attr('href');
+        let targetHref = $(this).attr('href');
 
         $('html, body').animate({
             scrollTop: $(targetHref).offset().top - 65
